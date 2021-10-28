4acb6afe4f262f576e7a7881f0993910482ff14a
natesol <natesoln@gmail.com>
2021-10-28 12:37:01 +0300

    Updates

---
+++ precache-manifest.dbbcec249cb60f1d5216a6aaab4d97db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23be63d5ba1287fd1af61023845c1b77",
+    "url": "/github.io/react-mui-covid19-data-app/index.html"
+  },
+  {
+    "revision": "f0314d28d850e5542ca1",
+    "url": "/github.io/react-mui-covid19-data-app/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "65d77bcea8f9cdc6bad5",
+    "url": "/github.io/react-mui-covid19-data-app/static/css/main.aacf4ab0.chunk.css"
+  },
+  {
+    "revision": "f0314d28d850e5542ca1",
+    "url": "/github.io/react-mui-covid19-data-app/static/js/2.71e34ee1.chunk.js"
+  },
+  {
+    "revision": "f4fbb9c35749914b4c7d298d36ddb8c4",
+    "url": "/github.io/react-mui-covid19-data-app/static/js/2.71e34ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d77bcea8f9cdc6bad5",
+    "url": "/github.io/react-mui-covid19-data-app/static/js/main.40a7494c.chunk.js"
+  },
+  {
+    "revision": "7c18a77c998173d535d5",
+    "url": "/github.io/react-mui-covid19-data-app/static/js/runtime-main.05942565.js"
+  }
+]);
